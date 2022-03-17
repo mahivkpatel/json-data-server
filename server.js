dcfc07584bccdf21a6c1a96025e68b1cfa72ac8c
mahivkpatel <mahivkpatel@gmail.com>
2022-03-17 11:55:38 +0530

    router change

--- server.js
+++ server.js
@@ -1,6 +1,6 @@
 const Jsonserver = require('json-server')
 const server = Jsonserver.create()
-const route = Jsonserver.router('db.json')
+const router = Jsonserver.router('db.json')
 
 const middlewares = jsonServer.defaults()
 const port = process.env.PORT || 3200
