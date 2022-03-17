2327ce27a5a1a55ba24fe29bd394e0db2700734f
mahivkpatel <mahivkpatel@gmail.com>
2022-03-17 12:09:15 +0530

    server changes

--- server.js
+++ server.js
@@ -1,9 +1,9 @@
-const Jsonserver = require('json-server')
-const server = Jsonserver.create()
-const router = Jsonserver.router('db.json')
-
+const jsonServer = require('json-server')
+const server = jsonServer.create()
+const router = jsonServer.router('db.json') // <== Will be created later
 const middlewares = jsonServer.defaults()
-const port = process.env.PORT || 3200
+const port = process.env.PORT || 3200 // <== You can change the port
+
 server.use(middlewares)
 server.use(router)
 
